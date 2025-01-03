12c52cbfe5cccc766d7edc062c5f7de3d6eb797e
Amit7366 <omit9090@gmail.com>
2025-01-03 21:22:34 +0600

    number section desktop view

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,30 +6,32 @@ const overlayMenu = document.querySelector(".overlay");
 
 // Open Close Navbar Menu on Click Burger
 if (burgerMenu && navbarMenu) {
-   burgerMenu.addEventListener("click", () => {
-      burgerMenu.classList.toggle("is-active");
-      navbarMenu.classList.toggle("is-active");
-   });
+    burgerMenu.addEventListener("click", () => {
+        burgerMenu.classList.toggle("is-active");
+        navbarMenu.classList.toggle("is-active");
+    });
 }
 
 // Close Navbar Menu on Click Links
 document.querySelectorAll(".menu-link").forEach((link) => {
-   link.addEventListener("click", () => {
-      burgerMenu.classList.remove("is-active");
-      navbarMenu.classList.remove("is-active");
-   });
+    link.addEventListener("click", () => {
+        burgerMenu.classList.remove("is-active");
+        navbarMenu.classList.remove("is-active");
+    });
 });
 
 // Fixed Navbar Menu on Window Resize
 window.addEventListener("resize", () => {
-   if (window.innerWidth >= 992) {
-      if (navbarMenu.classList.contains("is-active")) {
-         navbarMenu.classList.remove("is-active");
-         overlayMenu.classList.remove("is-active");
-      }
-   }
+    if (window.innerWidth >= 992) {
+        if (navbarMenu.classList.contains("is-active")) {
+            navbarMenu.classList.remove("is-active");
+            overlayMenu.classList.remove("is-active");
+        }
+    }
 });
 
+
+// dark and light mood toggle
 
 const toggleButton = document.getElementById('switch');
 const lightIcon = document.querySelector('.switch-light');
@@ -38,19 +40,19 @@ const darkIcon = document.querySelector('.switch-dark');
 // Check and apply the saved theme on page load
 const savedTheme = localStorage.getItem('theme');
 if (savedTheme === 'dark') {
-  document.body.classList.add('dark-mode');
-  lightIcon.style.display = 'none';
-  darkIcon.style.display = 'inline';
+    document.body.classList.add('dark-mode');
+    lightIcon.style.display = 'none';
+    darkIcon.style.display = 'inline';
 }
 
 // Toggle theme on button click
 toggleButton.addEventListener('click', () => {
-  const isDarkMode = document.body.classList.toggle('dark-mode');
+    const isDarkMode = document.body.classList.toggle('dark-mode');
 
-  // Toggle icon visibility
-  lightIcon.style.display = isDarkMode ? 'none' : 'inline';
-  darkIcon.style.display = isDarkMode ? 'inline' : 'none';
+    // Toggle icon visibility
+    lightIcon.style.display = isDarkMode ? 'none' : 'inline';
+    darkIcon.style.display = isDarkMode ? 'inline' : 'none';
 
-  // Save the theme preference
-  localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
+    // Save the theme preference
+    localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
 });
